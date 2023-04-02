9a370e6499f9e8a9b77c2bf197af672859b5d148
Alex01100101 <alexandru.telcean@ulbsibiu.ro>
2023-04-02 20:42:29 +0300

    Added the basic grid, made a FileDialog and made a very basic Parser

---
+++ Assembler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUSimulator
+{
+    public class Assembler
+    {
+        public List<string> ParseInstructionList(List<string> instructionList)
+        {
+            List<string> result = new List<string>();   
+            foreach(var instruction in instructionList)
+            {
+                if (instruction == "") continue;
+                var trimmedInstruction=instruction.ReplaceLineEndings().Trim('\t').Split(";").First();
+                if(trimmedInstruction.Contains(':') && trimmedInstruction.Last()!=':')
+                {
+                    result.Add(trimmedInstruction.Split(':').First() + ":");
+                    result.Add(trimmedInstruction.Split(':')[1].Trim(' '));
+                }
+                else 
+                {
+                    result.Add(trimmedInstruction);
+                }
+                
+            }
+            return result;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,77 @@ namespace CPUSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Button browseButton, assembleButton;
+        TextBox textBox, resultTextBox;
+        string fileName;
         public MainWindow()
         {
             InitializeComponent();
+            CreateControls();
+        }
+
+        public void CreateControls()
+        {
+            browseButton = new Button();
+            browseButton.SetValue(Grid.RowProperty, 1);
+            browseButton.SetValue(Grid.ColumnProperty, 3);
+            browseButton.Click += browseButton_OnClick;
+            browseButton.Content = "Browse";
+            assembleButton = new Button();
+            assembleButton.SetValue(Grid.RowProperty, 1);
+            assembleButton.SetValue(Grid.ColumnProperty, 5);
+            assembleButton.Click += assembleButton_OnClick;
+            assembleButton.Content = "Start assembler";
+            textBox = new TextBox();
+            textBox.SetValue(Grid.RowProperty, 1);
+            textBox.SetValue(Grid.ColumnProperty, 1);
+            textBox.IsEnabled = false;
+            resultTextBox = new TextBox();
+            resultTextBox.SetValue(Grid.RowProperty, 3);
+            resultTextBox.SetValue(Grid.ColumnProperty, 1);
+            Grid.SetColumnSpan(resultTextBox, 5);
+            resultTextBox.IsEnabled = false;
+            mainGrid.Children.Add(browseButton);
+            mainGrid.Children.Add(textBox);
+            mainGrid.Children.Add(resultTextBox);
+            mainGrid.Children.Add(assembleButton);
+        }
+
+        public void OpenDialog()
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if(openFileDialog.FileName.ToUpper().Contains(".ASM"))
+                {
+                    textBox.Text = openFileDialog.FileName;
+                }
+                else 
+                {
+                    textBox.Text = "Va rugam selectati un fisier cu extensia .asm";
+                }
+            }
+        }
+
+        public void browseButton_OnClick(object sender, EventArgs e)
+        {
+            OpenDialog();
+        }
+
+        public void assembleButton_OnClick(object sender, EventArgs e)
+        {
+            if (textBox.Text.Length == 0) return;
+            Assembler assembler = new Assembler();
+            var instructionList = new List<string>();
+
+            instructionList = File.ReadAllLines(textBox.Text).ToList();
+
+            var machineCode = assembler.ParseInstructionList(instructionList);
+
+            foreach(var instruction in machineCode)
+            {
+                resultTextBox.Text += instruction + '\r';
+            }
         }
     }
 }
