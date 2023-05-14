3018db0929cb9f6a9fcded6362d54b3f21eb003f
Zixtar <saftoiudaniel@yahoo.com>
2023-05-15 02:45:02 +0300

    Is the SEQ the brain of the hearth of the pc? Are we speaking metaphorically or logically?

---
+++ Simulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CPUSimulator.Globals;
+
+namespace CPUSimulator
+{
+    internal class Simulator
+    {
+        public Simulator()
+        {
+            var microprogram = File.ReadAllLines("Codificare.txt");
+            for (int i = 0; i < microprogram.Length; i++)
+            {
+                MPM[i] = Convert.ToInt64(microprogram[i], 2);
+            }
+        }
+
+        public void LoadProgram(List<int> program, short offset)
+        {
+            for (int i = 0; i < program.Count; i++)
+            {
+                MPM[i + offset] = Convert.ToInt64(program[i]);
+            }
+            PC = offset;
+        }
+
+
+    }
+
+}
