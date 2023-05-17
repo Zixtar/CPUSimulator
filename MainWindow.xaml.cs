7a3ce5483cbb284f7c164cf7d0a212b0e8f3af6d
Zixtar <saftoiudaniel@yahoo.com>
2023-05-17 23:38:46 +0300

    Moved UI to XAML, as God intended

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,54 +22,12 @@ namespace CPUSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
-        Button browseButton, assembleButton, nextButton;
-        TextBox textBox, resultTextBox, microprogramTextBox;
         List<int> lengths = new List<int>();
         List<int> indexes = new List<int>();
-        string fileName;
         Simulator simulator;
         public MainWindow()
         {
             InitializeComponent();
-            CreateControls();
-        }
-
-        public void CreateControls() //fereasca dumnezeu nu mai scrie UI in cod, pune-l in XAML
-        {
-            browseButton = new Button();
-            browseButton.SetValue(Grid.RowProperty, 1);
-            browseButton.SetValue(Grid.ColumnProperty, 3);
-            browseButton.Click += browseButton_OnClick;
-            browseButton.Content = "Browse";
-            nextButton = new Button();
-            nextButton.SetValue(Grid.RowProperty, 1);
-            nextButton.SetValue(Grid.ColumnProperty, 7);
-            nextButton.Click += nextButton_OnClick;
-            nextButton.Content = "Next";
-            assembleButton = new Button();
-            assembleButton.SetValue(Grid.RowProperty, 1);
-            assembleButton.SetValue(Grid.ColumnProperty, 5);
-            assembleButton.Click += assembleButton_OnClick;
-            assembleButton.Content = "Start assembler";
-            textBox = new TextBox();
-            textBox.SetValue(Grid.RowProperty, 1);
-            textBox.SetValue(Grid.ColumnProperty, 1);
-            textBox.IsEnabled = false;
-            resultTextBox = new TextBox();
-            resultTextBox.SetValue(Grid.RowProperty, 3);
-            resultTextBox.SetValue(Grid.ColumnProperty, 1);
-            resultTextBox.IsEnabled = false;
-            microprogramTextBox = new TextBox();
-            microprogramTextBox.SetValue(Grid.RowProperty, 3);
-            microprogramTextBox.SetValue(Grid.ColumnProperty, 3);
-            microprogramTextBox.IsEnabled = false;
-            Grid.SetColumnSpan(microprogramTextBox, 3);
-            mainGrid.Children.Add(browseButton);
-            mainGrid.Children.Add(textBox);
-            mainGrid.Children.Add(resultTextBox);
-            mainGrid.Children.Add(assembleButton);
-            mainGrid.Children.Add(microprogramTextBox);
-            mainGrid.Children.Add(nextButton);
         }
 
         public void OpenDialog()
@@ -87,13 +45,12 @@ namespace CPUSimulator
                 }
             }
         }
-
-        public void browseButton_OnClick(object sender, EventArgs e)
+        private void browseButton_Click(object sender, RoutedEventArgs e)
         {
             OpenDialog();
         }
 
-        public void assembleButton_OnClick(object sender, EventArgs e)
+        private void assembleButton_Click(object sender, RoutedEventArgs e)
         {
             if (textBox.Text.Length == 0) return;
 
@@ -134,9 +91,12 @@ namespace CPUSimulator
 
             simulator = new Simulator(machineCode);
             simulator.Start();
+
+            assembleButton.Visibility = Visibility.Hidden;
+            
         }
 
-        public void nextButton_OnClick(object sender, EventArgs e)
+        private void nextButton_Click(object sender, RoutedEventArgs e)
         {
             if (simulator == null) return;
             simulator.DoLoop();
@@ -146,8 +106,8 @@ namespace CPUSimulator
             if (simulator.Sequencer.stare == 2 || simulator.Sequencer.stare == 3)
             {
 
-                microprogramTextBox.SelectionStart = indexes[MAR-1] + MAR-1;
-                microprogramTextBox.SelectionLength = lengths[MAR-1];
+                microprogramTextBox.SelectionStart = indexes[MAR - 1] + MAR - 1;
+                microprogramTextBox.SelectionLength = lengths[MAR - 1];
             }
             else
             {
